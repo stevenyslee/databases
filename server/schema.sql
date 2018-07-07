5f37c0f33d4b87cd73136b06d18494481a18ac0e
stevenyslee <stevenyslee@gmail.com>
2018-07-06 17:23:38 -0700

    setup database tables and spec

--- server/schema.sql
+++ server/schema.sql
@@ -1,14 +1,34 @@
+DROP DATABASE chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
 
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE user (
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(30),
+  PRIMARY KEY (id)
+);
 
+CREATE TABLE roomname (
+  id INT NOT NULL AUTO_INCREMENT,
+  roomname VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  text VARCHAR(280),
+  roomname_id int NOT NULL,
+  user_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES user(id),
+  FOREIGN KEY (roomname_id) REFERENCES roomname(id)
+);
 
 
 /*  Execute this file from the command line by typing:
